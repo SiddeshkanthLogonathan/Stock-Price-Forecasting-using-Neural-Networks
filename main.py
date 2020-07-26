4ddd3a6358eadef218a48553bb107e4e0aecc8cc
SiddeshkanthLogonathan <logonathan@uni-potsdam.de>
2020-07-26 18:35:44 +0200

    refactored visualizing into a method

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import argparse as arg
 import sys
 
 def process_data(ticker):
+    print('Processing Data...')
     FData = FinancialDataLoader(ticker)
     data_iter = FinancialDataIterator(FData)
     train_iter = data_iter.partition_data(is_train=True)
     test_iter = data_iter.partition_data(is_train=False)
     return FData, train_iter, test_iter
+
+def visualize_data(ticker, curated_data):
+    print('Visualizing Data...')
+    FData_object = curated_data[0]
+    train_data = curated_data[1]
+    test_data = curated_data[2]
+
+    data_visualizer = FinancialDataVisualizer(train_data, test_data, FData_object)
+    data_visualizer.visualize(title=ticker)
 
 def main():
     ERROR_MESSAGE = 'Error: Please enter a ticker symbol. (ex: --ticker FB)'
@@ -19,12 +29,9 @@ def main():
 
     ticker = args.ticker
     curated_data = process_data(ticker=ticker)
-    FData_object = curated_data[0]
-    train_data = curated_data[1]
-    test_data = curated_data[2]
 
-    data_visualizer = FinancialDataVisualizer(train_data, test_data, FData_object)
-    data_visualizer.visualize(title=ticker)
+    visualize_data(ticker=ticker, curated_data=curated_data)
+
 
 if __name__ == '__main__':
     main()
